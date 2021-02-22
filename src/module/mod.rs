a1a5f35971649a167b32973f021c7d91f4ad025c
Clivern <hello@clivern.com>
2021-02-22 23:03:18 +0100

    update

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -3,3 +3,4 @@
 // license that can be found in the LICENSE file.
 
 pub mod command;
+pub mod writer;
--- src/module/writer.rs
+++ src/module/writer.rs
@@ -2,11 +2,32 @@
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
+use std::path::Path;
 
 pub struct Writer {
 
 }
 
 impl Writer {
+    pub fn new() -> Writer {
+        Writer {}
+    }
 
+	pub fn file_exists(&self, path: String) -> bool {
+	    let path = Path::new(path.as_str());
+
+	    if path.exists() {
+	        true
+	    } else {
+	       	false
+	    }
+	}
+}
+
+#[test]
+fn test_writer() {
+    let writer: Writer = Writer::new();
+
+    assert_eq!(writer.file_exists("cache/.gitignore".to_string()), true);
+    assert_eq!(writer.file_exists("cache/gitignore".to_string()), false);
 }
