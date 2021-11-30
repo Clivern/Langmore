fecd2db1d2f541151586fd1918dc0dd99a724233
Clivern <hello@clivern.com>
2021-11-30 22:50:03 +0100

    init

---
+++ src/test/java/com/clivern/meth/MethApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clivern.meth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
